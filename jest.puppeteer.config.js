89ce64eb94a55c1fd2e55b4b85a8b3f696aba5d7
Hiroaki Matoba <hiroaki.mtb@outlook.com>
2023-07-09 09:21:32 +0900

    update jest.puppeteer.config

--- jest.puppeteer.config.js
+++ jest.puppeteer.config.js
@@ -1,8 +1,6 @@
 module.exports = {
-  globalSetup: './tests/browser_tests/setup.js',
-  globalTeardown: './tests/browser_tests/teardown.js',
-  testEnvironment: './tests/browser_tests/puppeteer_environment.js',
-  testMatch: [
-    "**/tests/browser_tests/**/*.test.js",
-  ]
+  globalSetup: "./tests/browser_tests/setup.js",
+  globalTeardown: "./tests/browser_tests/teardown.js",
+  testEnvironment: "./tests/browser_tests/puppeteer_environment.js",
+  testMatch: ["**/tests/browser_tests/**/*.test.js"]
 };
